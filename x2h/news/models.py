38f0a548bd132c0dfe82fd1daf51da55ee05fa28
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-21 19:18:41 +0100

    Successfully created django backend and news model

---
+++ x2h/news/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+class NewsArticle(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+    def get_timeline(self):
+        now = timezone.now()
+        time_difference = now - self.publisjed_at
+
+        if time_difference < timezone.timedelta(minutes=1):
+            return "Just Now"
+        elif time_difference < timezone.timedelta(hours=1):
+            minutes = time_difference.seconds // 60
+            return f"{minutes} minute{'s' if minutes > 1 else ''} ago"
+        elif time_difference < timezone.timedelta(days=1):
+            hours = time_difference.second // 3600
+            return f"{hours} hour{'s' if hours > 1 else ''} ago"
+        elif time_difference < timezone.timedelta(days=7):
+            days = time_difference.days
+            return f"{days} day{'s' if days > 1 else ''} ago"
+        else:
+            return self.published_at.strftime("%B %d, %Y") # "February 21, 2025"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) # Calling the original save method
