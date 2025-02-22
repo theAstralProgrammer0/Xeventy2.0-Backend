2df5de20a9a8ec983e2f1f08509cecd4d2bd9268
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-22 01:14:53 +0100

    Configured Django app in settings to use locally hosted redis server

--- x2h/x2h/settings.py
+++ x2h/x2h/settings.py
@@ -29,17 +29,27 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'corsheaders',
+    'news',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
-    'news',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +74,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
 
 ROOT_URLCONF = 'x2h.urls'
 
