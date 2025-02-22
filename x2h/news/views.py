3c35bded2fd529648b4b13a527e740e5f9f645b4
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-02-22 01:27:30 +0100

    Modified news/views.py to implement server-side caching feature

--- x2h/news/views.py
+++ x2h/news/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.core.cache import cache
 from .models import NewsArticle, VideoNews
 from .serializers import NewsArticleSerializer, VideoNewsSerializer
 
@@ -6,13 +7,27 @@ from .serializers import NewsArticleSerializer, VideoNewsSerializer
 
 class LatestNewsArticleList(generics.ListAPIView):
     serializer_class = NewsArticleSerializer
-    # return only latest 8 articles
+    
     def get_queryset(self):
-        return NewsArticle.objects.all()[:8]
+        # Attempt to fetch cached news articles
+        cached_articles = cache.get("latest_news_articles")
+        if cached_articles is None:
+            # Query db for latest 8 articles
+            cached_articles = list(NewsArticle.objects.all()[:8])
+            # Cache the queryset for 5 mins (300s)
+            cache.set("latest_news_articles", cached_articles, 300)
+        return cached_articles
 
 
 class LatestVideoNews(generics.RetrieveAPIView):
     serializer_class = VideoNewsSerializer
+
     def get_object(self):
-        # return latest video news obj
-        return VideoNews.objects.first()
+        # Attempt to get cached news video
+        cached_video = cache.get("latest_video_news")
+        if cached_video is None:
+            # Query db to get latest video
+            cached_video = VideoNews.objects.first()
+            # Cache retrieved video
+            cache.set("latest_video_news", cached_video, 300)
+        return cached_video
